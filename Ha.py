6412b172d92019be35593ffc3538fc6f810f10c3
Abifitah <154583178+Abifitah@users.noreply.github.com>
2025-02-28 17:26:47 +0300

    Add files via upload

---
+++ Ha.py
@@ -0,0 +1,31 @@
+"""#One_hour=86400
+One_day=One_hour
+Week=7*One_day
+print("Seconds of the each week is:", Week)"""
+#program 1:
+'''Name=input( "Enter your name:" )
+School=input( "Enter your school name:" )
+Subject_1=int(input("Enter your score of your first subject:"))
+Subject_2=int(input("Enter your score of your second subject:"))
+Subject_3=int(input("Enter your score of your third subject:"))
+Total= Subject_1+Subject_2+Subject_3
+Average= Total/3
+print(Name)
+print(School)
+print('Your total subjects is:', Total)
+print('Your average of your subjects is:', Average)'''
+#Program 2:
+'''Item_1=int(input("Enter the price of item 1:"))
+Item_2=int(input("Enter the price of item 2:"))
+Item_3=int(input("Enter the price of item 3:"))
+Item_4=int(input("Enter the price of item 4:"))
+Item_5=int(input("Enter the price of item 5:"))
+Tota_of_the_sale= Item_1+Item_2+Item_3+Item_4+Item_5
+print("Total of the whole sale is:", Tota_of_the_sale,"$", sep="")'''
+#Program 3
+Distance_1= 6*70
+print("The car will travel by distance in 6 hours ",Distance_1,"Km", sep="")
+Distance_2=10*70
+print("The car will travel by distance in 10 hours ",Distance_2,"Km", sep="")
+Distance_3= 15*70
+print("The car will travel by distance in 15 hours ",Distance_3,"Km", sep="")
